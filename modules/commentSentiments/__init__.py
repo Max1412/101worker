7bfd5888ebc450da1bb39a6741df86d38febc58f
Anna Schneider <anschneider@uni-koblenz.de>
2016-06-13 15:27:37 +0200

    add commentSentiments module

---
+++ modules/commentSentiments/__init__.py
@@ -0,0 +1,30 @@
+config = {
+    'wantdiff': False,
+    'wantsfiles': False,
+    'threadsafe': True,
+    }
+
+}
+
+import nltk
+
+
+def run(context):
+
+
+
+
+class CommentSentiment(unittest.TestCase):
+
+	 def setUp(self):
+    	self.env=Mock()
+
+
+
+
+
+
+
+def test():
+	suite = unittest.TestLoader().loadTestsFromTestCase(ContentSentiment)
+    unittest.TextTestRunner(verbosity=2).run(suite)
