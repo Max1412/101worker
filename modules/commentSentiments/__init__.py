e53a54dadc15a1374a3552196371fdcd8fcbcac2
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2016-06-29 16:01:10 +0200

    Implement dump creation for langs and devs
    Update plots to also plot non neutral data plots.
    Language and Developer dumps are equal to contribution dumps in terms of
    structure.

--- modules/commentSentiments/__init__.py
+++ modules/commentSentiments/__init__.py
@@ -3,8 +3,9 @@ config = {
     'wantsfiles': False,
     'threadsafe': True,
     'behavior': {
-        'creates': [['dump', 'commentSentiments']],
-        'uses': [['dump', 'contributionCommentFilter']]
+        'creates': [['dump', 'languageCommentSentiments'],
+        ['dump', 'developerCommentSentiments']],
+        'uses': [['dump', 'contributionCommentSentiments']]
     }
 }
 
@@ -12,7 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-def plotSentiments(pp, title, with_neutral, data, total_scores):
+def plotSentiments(pp, title, ylabel, with_neutral, data, total_scores, use_non_neutral_scores=False):
     neutrals = []
     compounds = []
     negatives = []
@@ -20,6 +21,8 @@ def plotSentiments(pp, title, with_neutral, data, total_scores):
     legend = []
     for l, item_data in data.items():
         average_scores_data = item_data['averageScores']
+        if use_non_neutral_scores:
+            average_scores_data = item_data['averageNonNeutral']
         if average_scores_data['neu'] != average_scores_data['neg'] != average_scores_data['pos'] != average_scores_data['compound'] != 0:
             neutrals += [average_scores_data['neu']]
             compounds += [average_scores_data['compound']]
@@ -37,8 +40,8 @@ def plotSentiments(pp, title, with_neutral, data, total_scores):
     width = 1.0/(1.0+elements)
     ind = np.arange(N)
     if with_neutral:
-        neutral_bar = ax.bar(ind, neutrals, width, color='r')
-        ax.axhline(total_scores['neu'], color='r', alpha=0.5)
+        neutral_bar = ax.bar(ind, neutrals, width, color='y')
+        ax.axhline(total_scores['neu'], color='y', alpha=0.5)
         offset += width
     positive_bar = ax.bar(ind + offset, positives, width, color='g')
     ax.axhline(total_scores['pos'], color='g', alpha=0.5)
@@ -48,10 +51,10 @@ def plotSentiments(pp, title, with_neutral, data, total_scores):
     offset += width
     compound_bar = ax.bar(ind + offset, compounds, width, color='b')
     ax.axhline(total_scores['compound'], color='b', alpha=0.5)
-    ax.set_ylabel('Average scores')
+    ax.set_ylabel(ylabel)
     ax.set_title(title)
     ax.set_xticks(ind + elements/2.*width)
-    lsize = 1.0/N * 8.0 + 10.0
+    lsize = 1.0/N * 10.0 + 5.0
     ax.set_xticklabels(legend, fontsize=lsize)
     ax.yaxis.grid(True, linestyle='--', which='major',
                    color='grey', alpha=.25)
@@ -60,7 +63,6 @@ def plotSentiments(pp, title, with_neutral, data, total_scores):
     else:
         fig.legend((positive_bar[0], negative_bar[0], compound_bar[0]), ('positive', 'negative', 'compound'), 'upper right')
     ax.autoscale_view()
-    #plt.show()
     plt.savefig(pp, format='pdf')
 
 def run(context):
@@ -73,104 +75,145 @@ def run(context):
     else:
         pages = []
 
-# What this does: 1. Get list of all programming languages:
-#                     (loop through namespace language, select those who are
-# "programming" languages)
-#                 2. Loop through contributions in wiki pages (see below) and
-# search for "Uses::Language:<language>"
-
-    languages = []
+    # Only consider languages for language classification that are programming languages
+    programming_languages = []
     for item in pages:
         if "p" in item.keys() and item["p"] == "Language":
            if "n" in item.keys() and "internal_links" in item.keys():
                 title = item["n"]
                 for x in item["internal_links"]:
                     if "programming language" in x:
-                        languages += [title]
+                        programming_languages += [title]
                         break;
 
-    contributions = {}
+    # Extract languages and developers per contribution from wiki-links dump
+    languages = {}
     devs = {}
     for item in pages:
         if "p" in item.keys() and item["p"] == "Contribution":
             if "n" in item.keys():
                 if "internal_links" in item.keys():
                     title = item["n"]
-                    contributions[title] = []
-                    for l in languages:
-                        contributions[title] += [l] if "Uses::Language:"+l in item["internal_links"] else []
+                    languages[title] = []
+                    for l in programming_languages:
+                        languages[title] += [l] if "Uses::Language:"+l in item["internal_links"] else []
                 if "DevelopedBy" in item.keys():
                     devs[title] = []
                     for dev in item["DevelopedBy"]:
                         devs[title] += [dev['n']]
 
-    contribution_sentiments = context.read_dump('contributionCommentFilter')
+    contribution_sentiments = context.read_dump('contributionCommentSentiments')
     lang_num_sentences = {}
+    lang_num_non_neutral_sentences = {}
+    lang_num_too_short_sentences = {}
+    lang_scores_non_neutral = {}
     lang_scores = {}
     dev_num_sentences = {}
+    dev_num_non_neutral_sentences = {}
+    dev_num_too_short_sentences = {}
+    dev_scores_non_neutral = {}
     dev_scores = {}
+    # total scores are used to create an overall average displayed in the bar
+    # chart plotted to simplify grading of displayed data in realtion to overall
+    # data in the chart.
     total_scores = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+    total_non_neutral_scores = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
     total_sentences = 0
+    total_non_neutral_sentences = 0
+    # Create data corresponding to contribution_sentiments but classified by
+    # programming language and by developer of the contribution.
+    language_sentiments = {}
+    developer_sentiments = {}
     for c, data in contribution_sentiments.items():
         total_sentences += data['sentences']
+        total_non_neutral_sentences += data['nonNeutralSentences']
         for k in total_scores:
             total_scores[k] += data['scores'][k]
-        lang = contributions[c]
+            total_non_neutral_scores[k] += data['nonNeutralScores'][k]
+        lang = languages[c]
         for l in lang:
-            if not l in lang_num_sentences:
-                lang_num_sentences[l] = 0
-            lang_num_sentences[l] += data['sentences']
-            if not l in lang_scores:
-                lang_scores[l] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
-            for k in lang_scores[l]:
-                lang_scores[l][k] += data['scores'][k]
+            if not l in language_sentiments:
+                language_sentiments[l] = {}
+                language_sentiments[l]['sentences'] = 0
+                language_sentiments[l]['tooShortSentences'] = 0
+                language_sentiments[l]['nonNeutralSentences'] = 0
+                language_sentiments[l]['scores'] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+                language_sentiments[l]['nonNeutralScores'] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+            language_sentiments[l]['sentences'] += data['sentences']
+            language_sentiments[l]['tooShortSentences'] += data['tooShortSentences']
+            language_sentiments[l]['nonNeutralSentences'] += data['nonNeutralSentences']
+            for k in language_sentiments[l]['scores']:
+                language_sentiments[l]['scores'][k] += data['scores'][k]
+                language_sentiments[l]['nonNeutralScores'][k] += data['nonNeutralScores'][k]
         dev = devs[c]
-        for d in dev:
-            if not d in dev_num_sentences:
-                dev_num_sentences[d] = 0
-            dev_num_sentences[d] += data['sentences']
-            if not d in dev_scores:
-                dev_scores[d] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
-            for k in dev_scores[d]:
-                dev_scores[d][k] += data['scores'][k]
+        for l in dev:
+            if not l in developer_sentiments:
+                developer_sentiments[l] = {}
+                developer_sentiments[l]['sentences'] = 0
+                developer_sentiments[l]['tooShortSentences'] = 0
+                developer_sentiments[l]['nonNeutralSentences'] = 0
+                developer_sentiments[l]['scores'] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+                developer_sentiments[l]['nonNeutralScores'] = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+            developer_sentiments[l]['sentences'] += data['sentences']
+            developer_sentiments[l]['tooShortSentences'] += data['tooShortSentences']
+            developer_sentiments[l]['nonNeutralSentences'] += data['nonNeutralSentences']
+            for k in developer_sentiments[l]['scores']:
+                developer_sentiments[l]['scores'][k] += data['scores'][k]
+                developer_sentiments[l]['nonNeutralScores'][k] += data['nonNeutralScores'][k]
     for t in total_scores:
         if total_sentences == 0:
             total_scores[t] = 0
         else:
             total_scores[t] = total_scores[t]/total_sentences
-    language_sentiments = {}
-    for l in lang_num_sentences:
-        language_sentiments[l] = {}
+        if total_non_neutral_sentences == 0:
+            total_non_neutral_scores[t] = 0
+        else:
+            total_non_neutral_scores[t] = total_non_neutral_scores[t]/total_non_neutral_sentences
+
+    for l in language_sentiments:
         average_scores = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+        average_non_neutral = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
         for key in average_scores:
-            if lang_num_sentences[l] == 0:
+            if language_sentiments[l]['sentences'] == 0:
                 average_scores[key] = 0
+                average_non_neutral[key] = 0
             else:
-                average_scores[key] = lang_scores[l][key]/lang_num_sentences[l]
+                average_scores[key] = language_sentiments[l]['scores'][key]/language_sentiments[l]['sentences']
+                average_non_neutral[key] = language_sentiments[l]['nonNeutralScores'][key]/language_sentiments[l]['nonNeutralSentences']
         language_sentiments[l]['averageScores'] = average_scores
-    developer_sentiments = {}
-    for d in dev_num_sentences:
-        developer_sentiments[d] = {}
+        language_sentiments[l]['averageNonNeutral'] = average_non_neutral
+
+    for l in developer_sentiments:
         average_scores = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
+        average_non_neutral = {"compound": 0, "pos": 0, "neu": 0, "neg": 0}
         for key in average_scores:
-            if dev_num_sentences[d] == 0:
+            if developer_sentiments[l]['sentences'] == 0:
                 average_scores[key] = 0
+                average_non_neutral[key] = 0
             else:
-                average_scores[key] = dev_scores[d][key]/dev_num_sentences[d]
-        developer_sentiments[d]['averageScores'] = average_scores
+                average_scores[key] = developer_sentiments[l]['scores'][key]/developer_sentiments[l]['sentences']
+                average_non_neutral[key] = developer_sentiments[l]['nonNeutralScores'][key]/developer_sentiments[l]['nonNeutralSentences']
+        developer_sentiments[l]['averageScores'] = average_scores
+        developer_sentiments[l]['averageNonNeutral'] = average_non_neutral
 
     pp = PdfPages(context.get_env('dumps101dir') + '/multipage.pdf')
-    plotSentiments(pp, "Contribution comment sentiments", False, contribution_sentiments, total_scores)
-    plotSentiments(pp, "Language comment sentiments", False, language_sentiments, total_scores)
-    plotSentiments(pp, "Developer comment sentiments", False, developer_sentiments, total_scores)
+    plotSentiments(pp, "Contribution comment sentiments", "Average Scores", False, contribution_sentiments, total_scores)
+    plotSentiments(pp, "Non neutral contribution comment sentiments", "Average Scores", True, contribution_sentiments, total_non_neutral_scores, True)
+    plotSentiments(pp, "Language comment sentiments", "Average Scores", False, language_sentiments, total_scores)
+    plotSentiments(pp, "Non neutral language comment sentiments", "Average Scores", True, language_sentiments, total_non_neutral_scores, True)
+    plotSentiments(pp, "Developer comment sentiments", "Average Scores", False, developer_sentiments, total_scores)
+    plotSentiments(pp, "Non neutral developer comment sentiments", "Average Scores", True, developer_sentiments, total_non_neutral_scores, True)
 
     pp.close()
+
+    context.write_dump('developerCommentSentiments', developer_sentiments)
+    context.write_dump('languageCommentSentiments', language_sentiments)
 
 import unittest
 from unittest.mock import Mock, patch
 
 
-class ProgrammingLanguagePerContribution(unittest.TestCase):
+class CommentSentiments(unittest.TestCase):
 
     def setUp(self):
         self.env = Mock()
@@ -230,5 +273,5 @@ class ProgrammingLanguagePerContribution(unittest.TestCase):
         self.env.write_dump.assert_called_with('programmingLanguagePerContribution', {})
 
 def test():
-    suite = unittest.TestLoader().loadTestsFromTestCase(ProgrammingLanguagePerContribution)
+    suite = unittest.TestLoader().loadTestsFromTestCase(CommentSentiments)
     unittest.TextTestRunner(verbosity=2).run(suite)
